6280bc04016f839cf1078030a152bef15b06a73e
TalhaNaseer987 <talhanaseer987@gmail.com>
2022-04-28 11:43:40 +0500

    Flutter-Assignment

---
+++ Flutter-Assignment/flutter_assignment_q_01/assignment/lib/story.dart
@@ -0,0 +1,11 @@
+class Story {
+  String? st;
+  String? c1;
+  String? c2;
+
+  Story({ required String storyTitle,required String choice1,required String choice2}) {
+    st = storyTitle;
+    c1 = choice1;
+    c2 = choice2;
+  }
+}
